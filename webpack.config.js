d9abe3a7c059a78dcd636361e1bdc135229c698e
Kirill Tikhonov <lliriq@me.com>
2021-11-14 12:19:46 +0300

    lint-staged

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { resolve } = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { resolve } = require("path");
 
 module.exports = {
-    entry: resolve(__dirname, './src/index.js'),
-    output: {
-        filename: 'bundle.js',
-        path: resolve(__dirname + '/dist'),
-        clean: true,
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: resolve(__dirname, 'index.html'),
-    })],
+  entry: resolve(__dirname, "./src/index.js"),
+  output: {
+    filename: "bundle.js",
+    path: resolve(`${__dirname}/dist`),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, "index.html"),
+    }),
+  ],
 };
