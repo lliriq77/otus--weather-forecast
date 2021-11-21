63e81753d10c07b645e709732a2704a20ff8221e
Kirill Tikhonov <lliriq@me.com>
2021-11-21 18:04:18 +0300

    Add history

--- src/createMarkUp.js
+++ src/createMarkUp.js
@@ -23,20 +23,38 @@ export async function createMurkUp(el) {
   }
 
   async function getWeather() {
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${input.value}&units=metric&appid=92dae48bac98a7191c6227716a76ac12`;
+    const inputValue = input.value || localCity.city;
+    const apiKey = "92dae48bac98a7191c6227716a76ac12";
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&units=metric&appid=${apiKey}`;
     const response = await fetch(url);
-    const weather = await response.json();
-    const temp = weather.list[0].main.temp.toFixed(0);
-    const city = weather.city.name;
-    const { icon } = weather.list[0].weather[0];
-    const image = `<image src=https://openweathermap.org/img/wn/${icon}@2x.png>`;
+    weather = await response.json();
+    return weather;
+  }
+
+  async function buttonEvent() {
+    await getWeather();
+    // console.log(weather);
+    temp = weather.list[0].main.temp.toFixed(0);
+    city = weather.city.name;
+    icon = weather.list[0].weather[0].icon;
+    image = `<image src=https://openweathermap.org/img/wn/${icon}@2x.png>`;
     history.push(city);
     saveHistory(history);
     drawHistory(hist, history);
     rslt.innerHTML = `${city} ${temp}°C ${image}`;
     input.value = "";
   }
+
   let history = await readHistory();
+  const localCityResponse = await fetch("https://get.geojs.io/v1/ip/geo.json");
+  const localCity = await localCityResponse.json();
+  let weather = await getWeather();
+  let temp = weather.list[0].main.temp.toFixed(0);
+  let city = weather.city.name;
+  let {icon} = weather.list[0].weather[0];
+  let image = `<image src=https://openweathermap.org/img/wn/${icon}@2x.png>`;
+  rslt.innerHTML = `${city} ${temp}°C ${image}`;
+
   drawHistory(hist, history);
-  button.addEventListener("click", getWeather);
+  button.addEventListener("click", buttonEvent);
 }
