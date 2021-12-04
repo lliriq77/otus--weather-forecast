7e33cd73b9454f55f30999443888738d1a1a567a
Kirill Tikhonov <lliriq@me.com>
2021-12-04 15:30:33 +0300

    drawHistory fix

--- src/drawHistory.js
+++ src/drawHistory.js
@@ -1,5 +1,5 @@
 export function drawHistory(elTwo, arr) {
-  elTwo.innerHTML = `HISTORY:${arr
+  elTwo.innerHTML = `${arr
     .map((item) => `<span style="margin: 2px;">${item}</span>`)
     .reverse()
     .join("")}`;
